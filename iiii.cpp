f49f9ebe468629c3c99eb754e915c0e543f4e1fe
Irfanmukasyaf <imukasyaf@gmail.com>
2025-02-26 23:31:01 +0700

    first commit

---
+++ iiii.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    struct Buku {
+        string nama;
+        
+
+    }
+
+    {
+        /* data */
+    };
+    
+
+    Buku Prog[5] = {
+        {"PH", 10000},
+        {"JAVA", 20000},
+        {"C++", 30000},
+        {"PYTHON", 40000},
+        {"HTML", 50000},
+    };
+    Buku Cerita[5] = {
+        {"Buku A", 20000},
+        {"Buku B", 10000},
+        {"Buku C", 40000},
+        {"Buku D", 50000},
+        {"Buku E", 30000},
+
+
+
+    }
+    }
+}
---
+++ pppp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka[] = {7,11,10,29,96,54}
+    
+}
+
+
+
+Binary search
+int angka[] ={80, 78, 55, 10, 90, 53, 89};
+int awal =  0, tengah;
+int akhir = (sizeof(angka)/sizeof(*angka))
